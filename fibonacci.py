e285f16cf7a961cb4d833966ba7bceddc0caabb2
Ömer F. CAN <53655637+furcanomer@users.noreply.github.com>
2022-07-04 17:49:41 +0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+#fibonacci sayıları kodu
+
+a = 0
+b = 1
+print(a)
+while b < 255:  # b sayısını 255 ile sınırladım.
+    print(b)    # döngünün her bir adımında b sayısının son hali ekrana basılıyor.
+    b = a + b   # kendinden önceki iki sayının toplamını b ye eşitledim.
+    a = b - a   # yeni oluşan b nin kendinden bir önceki sayı eski b
+                # eski b yeni oluşan b ye a nın eklenmemiş halidir.
+                # bu yüzden yeni b den eski a yı çıkarıp yeni a yı oluşturdum.
+
+# instagram.com/furcanomer
+# genellikle istekleri kabul etmem
