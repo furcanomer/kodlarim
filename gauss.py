e7b36e33b6ebb3db6358da3a08269ac9d1e616cb
Ömer F. CAN <53655637+furcanomer@users.noreply.github.com>
2022-11-05 20:42:34 +0300

    Add files via upload

---
+++ gauss.py
@@ -0,0 +1,13 @@
+def gauss(n):
+    return int((n*(n+1))/2)
+
+print("Gauss 1.0\n")
+
+while(True):
+
+    
+    sayi = int(input("Sayı Giriniz: "))
+
+    sonuc = gauss(sayi)
+
+    print("1'den "+str(sayi)+" sayısına kadar olan sayıların toplamı: "+str(sonuc)+"\n")
