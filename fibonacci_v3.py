effab9d0497a74f20844801281f36e69822d07d5
Ömer F. CAN <53655637+furcanomer@users.noreply.github.com>
2023-02-28 15:29:37 +0300

    Update fibonacci_v3.py

--- fibonacci_v3.py
+++ fibonacci_v3.py
@@ -1,6 +1,6 @@
 # fibonacci sayıları kodu
 # 3. versiyon
-# instagram.com/furcanomer
+# twitter.com/omer_can80
 while True: # programın ana döngüsü
     try:    # Hata kontrolü yaptım. Kullanıcı, sayı girilmesi gereken yere yazı yazabilir.
         i = float(input("Fibonacci dizisi sınırını belirleyin: "))  # istediğimiz sayı tipini gerçek sayı (float) yaptım
