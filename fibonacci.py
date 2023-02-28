6ffbd0bf6de3e9f9d77c8fd8a1ff9f5e4c180132
Ömer F. CAN <53655637+furcanomer@users.noreply.github.com>
2023-02-28 15:30:17 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -9,5 +9,3 @@ while b < 255:  # b sayısını 255 ile sınırladım.
     a = b - a   # yeni oluşan b nin kendinden bir önceki sayı eski b
                 # eski b yeni oluşan b ye a nın eklenmemiş halidir.
                 # bu yüzden yeni b den eski a yı çıkarıp yeni a yı oluşturdum.
-
-# instagram.com/furcanomer
