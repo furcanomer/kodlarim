0490147b0eee0a538562dea0ade361811d1396bf
Ömer F. CAN <53655637+furcanomer@users.noreply.github.com>
2023-04-19 11:28:43 +0300

    Update sayi_tahmin_oyunu.py

--- sayi_tahmin_oyunu.py
+++ sayi_tahmin_oyunu.py
@@ -1,21 +1,35 @@
 import os
-baslik = "Sayı Tahmin Oyunu 1.0\n"
+baslik = "Sayı Tahmin Oyunu 2.0\n"
 print(baslik)
-print("""Sayıyı aklında tutan kişi, aklında tuttuğu sayıyı programa girer.
+print("""Sayıyı aklında tutan kişi, aklında tuttuğu sayıyının, hangi sayılar aralığında olduğunu
+belirtir. Sonra aklında tuttuğu sayıyı programa girer.
 Sonra program, akılda tutulan sayıyı kendi zekasında tutar ve ekrandan gizler.
-Sonra tahmin etmeye çalışan oyuncu veya oyuncular
+Sonra sayıyı tahmin etmeye çalışan oyuncu veya oyuncular
 ekrana tahmin ettikleri sayıları girerler.
 Eğer başarılı olurlarsa oyun biter.
 Eğer yanlış tahmin ederlerse oyun, doğru tahmin edilene kadar devam eder.
 """)
-tutulan_sayi = int(input("Tuttuğun sayıyı gir: "))
-os.system("cls")
-print(baslik)
-while True:
-    sayi = int(input("Sayıyı tahmin et: "))
-    if (sayi == tutulan_sayi):
-        print("Bravo Sayıyı doğru tahmin ettin\n.")
-        break
+bas = int(input("Tuttuğun sayı, hangi sayılar arasında?\nAralığın ilk değeri: "))
+bit = int(input("Aralığın son son değeri: "))
+while True: 
+    tutulan_sayi = int(input("Tuttuğun sayıyı gir: "))
+    if tutulan_sayi >= bas and tutulan_sayi <= bit:
+        os.system("cls")
+        print(baslik)
+        print("Tahmin edeceğin sayı,",bas,"ve",bit,"aralığında olmalıdır.")
+        while True:
+            sayi = int(input("Sayıyı tahmin et: "))
+            if (sayi == tutulan_sayi):
+                print("Bravo Sayıyı doğru tahmin ettin.\n")
+                break
+            elif sayi < bas or sayi > bit:
+                print("Tahmin ettiğin sayı,",bas,"ve",bit,"aralığında değil.\nTekrar dene.")
+                continue
+            else:
+                print("Yanlış tahmin. Tekrar dene.")
+                continue
     else:
+        print("Tuttuğun sayı,",bas,"ve",bit,"aralığında değil.\nTahmin ettiğin sayıyı tekrar gir.")
         continue
+    break
 a = input("Oyundan çıkmak için 'Enter' tuşuna basabilirsin\n")
