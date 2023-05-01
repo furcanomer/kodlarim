783382920d9684b69378f311bd215651b94ec724
Ömer F. CAN <53655637+furcanomer@users.noreply.github.com>
2023-05-01 03:10:44 +0300

    Update ikizasallar.py

--- ikizasallar.py
+++ ikizasallar.py
@@ -5,7 +5,7 @@
 # birer birer ilerler ve ikiz asalları üretir
 # ürettiği tüm sayıları da programı çalıştırdığınız
 # dizinde bir text dosyasına kaydeder
-
+import os
 def asalmi(x):
     if x == 2:
         return 1
@@ -17,16 +17,28 @@ def asalmi(x):
                 return 0
            
         return 1
-print("ikiz_asallar 1.0")
-print("https://github.com/furcanomer\n")         
-sayi = int(input("Başlangıç sayısını gir: "))
+print("ikiz_asallar 1.1")
+print("https://github.com/furcanomer\n")
+
+while True: 
+    dosya_adi = input("oluşturulacak dosya adı ne olsun: ")
+    dosyam = dosya_adi+".txt"
+    if os.path.isfile(dosyam):
+        print("böyle bir dosya var. başka bir dosya adı girin.")
+    else:
+        dosya = open(dosyam,"a")
+        print("Tamamdır. Dosya başarıyla oluşturuldu.")
+        break
+
+sayi = int(input("Başlangıç sayısını girin: "))
 sayi2 = sayi + 2
+
+
 
 while True:
     if asalmi(sayi) and asalmi(sayi2):
         print(sayi,sayi2)
         yazi = (str(sayi)+" "+str(sayi2)+"\n")
-        dosya = open("ikiz_asallar.txt","a")
         dosya.write(yazi)
     else:
         pass
